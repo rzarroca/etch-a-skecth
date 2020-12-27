2903fd8b097492e0a9d09e59102bb05ada0e0aa6
ricardozarroca <ricardozarroca@gmail.com>
2020-12-27 19:36:26 +0000

    Functions reorganization and Code cleaning

--- script.js
+++ script.js
@@ -1,28 +1,11 @@
-// Divs and constants creation
+// Variables and constants declaration, Board creation
 const container = document.querySelector('.container');
-let arrayDiv = [];
-
-let sizeNumber = 4;
-
 const doc = document.documentElement;
-doc.style.setProperty('--size', sizeNumber);
+let arrayDiv = [];
+let sizeNumber = 4;
+createBoard();
 
-for (let i = 0; i < (sizeNumber*sizeNumber); i++) {
-  arrayDiv[i] = document.createElement('div');
-  arrayDiv[i].className = 'grid';
-  container.appendChild(arrayDiv[i]);
-};
-
-
-// Change board color
-arrayDiv.forEach((div) => {
-  div.addEventListener('mouseover', function(e) {
-    this.classList.add('colored');
-  });
-});
-
-
-// Buttons events
+// Buttons events declaration
 const buttons = document.querySelectorAll('button');
 
 buttons.forEach((button) => {
@@ -31,35 +14,49 @@ buttons.forEach((button) => {
       case 'reset':
         resetBoard();
         break;
-      case 'color':
+      case 'random':
         alert('Button clicked: ' + e.target.name);
         pickColor();
         break;
       case 'resize':
         changeSize();
         break;
-    };
+    }
   });
 });
 
+// create Board and event declaration
+function createBoard() {
+  //set variables for css grid
+  doc.style.setProperty('--size', sizeNumber);
+
+  //create board
+  for (let i = 0; i < (sizeNumber*sizeNumber); i++) {
+    arrayDiv[i] = document.createElement('div');
+    arrayDiv[i].className = 'grid';
+    container.appendChild(arrayDiv[i]);
+  }
+
+  // Change board color if mouse is over
+  arrayDiv.forEach((div) => {
+    div.addEventListener('mouseover', function(e) {
+      this.style.backgroundColor = 'rgba(0, 0, 0, 0.1)';
+    });
+  });
+}
 
 // Reset Board
 function resetBoard() {
-  arrayDiv.forEach((div) => {
-    div.classList.remove('colored');
-  });
-};
-
+  arrayDiv.forEach(div => div.removeAttribute('style'));
+}
 
 // Resize Board
 function changeSize() {
   container.textContent = '';
   arrayDiv = [];
-  sizeNumber = prompt('Choose the number of rows/columns of the board', '5');
-  doc.style.setProperty('--size', sizeNumber);
-  for (let i = 0; i < (sizeNumber*sizeNumber); i++) {
-    arrayDiv[i] = document.createElement('div');
-    arrayDiv[i].className = 'grid';
-    container.appendChild(arrayDiv[i]);
-  };
-};
+  sizeNumber = prompt('Choose the number of rows/columns of the board', 'Max. allowed: 60');
+  if (sizeNumber > 60) {
+    sizeNumber = 60;
+  }
+  createBoard();
+}
