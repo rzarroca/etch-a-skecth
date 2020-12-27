dd521bf479f1d20af21a95e61ba3ff58705fdead
ricardozarroca <ricardozarroca@gmail.com>
2020-12-27 04:32:54 +0000

    Buttons added, css grid generated, reset and changeSize functions made.

--- script.js
+++ script.js
@@ -1,10 +1,65 @@
+// Divs and constants creation
 const container = document.querySelector('.container');
-const arrayDiv = [];
+let arrayDiv = [];
 
-for (let i = 0; i < 16; i++) {
+let sizeNumber = 4;
+
+const doc = document.documentElement;
+doc.style.setProperty('--size', sizeNumber);
+
+for (let i = 0; i < (sizeNumber*sizeNumber); i++) {
   arrayDiv[i] = document.createElement('div');
   arrayDiv[i].className = 'grid';
   container.appendChild(arrayDiv[i]);
-}
+};
 
-console.log(container);
+
+// Change board color
+arrayDiv.forEach((div) => {
+  div.addEventListener('mouseover', function(e) {
+    this.classList.add('colored');
+  });
+});
+
+
+// Buttons events
+const buttons = document.querySelectorAll('button');
+
+buttons.forEach((button) => {
+  button.addEventListener('click', (e) => {
+    switch (e.target.name) {
+      case 'reset':
+        resetBoard();
+        break;
+      case 'color':
+        alert('Button clicked: ' + e.target.name);
+        pickColor();
+        break;
+      case 'resize':
+        changeSize();
+        break;
+    };
+  });
+});
+
+
+// Reset Board
+function resetBoard() {
+  arrayDiv.forEach((div) => {
+    div.classList.remove('colored');
+  });
+};
+
+
+// Resize Board
+function changeSize() {
+  container.textContent = '';
+  arrayDiv = [];
+  sizeNumber = prompt('Choose the number of rows/columns of the board', '5');
+  doc.style.setProperty('--size', sizeNumber);
+  for (let i = 0; i < (sizeNumber*sizeNumber); i++) {
+    arrayDiv[i] = document.createElement('div');
+    arrayDiv[i].className = 'grid';
+    container.appendChild(arrayDiv[i]);
+  };
+};
