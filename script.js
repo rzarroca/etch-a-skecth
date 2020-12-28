658518a0f43127d4a20bb00fbf44d0599dd1265d
ricardozarroca <ricardozarroca@gmail.com>
2020-12-28 23:50:01 +0000

    Added random color button and its functionality

--- script.js
+++ script.js
@@ -2,30 +2,34 @@
 const container = document.querySelector('.container');
 const doc = document.documentElement;
 let arrayDiv = [];
-let sizeNumber = 4;
+let sizeNumber = 10;
+let color = 'gray';
 createBoard();
 
 // Buttons events declaration
 const buttons = document.querySelectorAll('button');
-
 buttons.forEach((button) => {
   button.addEventListener('click', (e) => {
     switch (e.target.name) {
       case 'reset':
         resetBoard();
         break;
-      case 'random':
-        alert('Button clicked: ' + e.target.name);
-        pickColor();
-        break;
       case 'resize':
         changeSize();
+        break;
+      case 'gray':
+        color = 'gray';
+        resetBoard();
+        break;
+      case 'random':
+        color = 'random';
+        resetBoard();
         break;
     }
   });
 });
 
-// create Board and event declaration
+// create Board and board event declaration
 function createBoard() {
   //set variables for css grid
   doc.style.setProperty('--size', sizeNumber);
@@ -37,32 +41,45 @@ function createBoard() {
     container.appendChild(arrayDiv[i]);
   }
 
-  // Change board color if mouse is over
+  // Crate event on divs
   arrayDiv.forEach((div) => {
-    div.addEventListener('mouseover', function(e) {
-      if (this.style.backgroundColor.match(/rgba/)) {
-        let opacity = Number(this.style.backgroundColor.slice(-4,-1));
-        if (opacity < 0.9) {
-          opacity += 0.1;
-          this.style.backgroundColor = `rgba(0, 0, 0, ${opacity})`;
-        }
-      } else {
-          this.style.backgroundColor = `rgba(0, 0, 0, 0.1)`;
-      };
-    });
+    div.addEventListener('mouseover', paintBoard);
   });
 }
 
-// Reset Board
+// Set how to paint the board
+function paintBoard(e) {
+  if (color == 'gray') {
+    if (this.style.backgroundColor.match(/rgba/)) {
+      let opacity = Number(this.style.backgroundColor.slice(-4,-1));
+      if (opacity < 0.9) {
+        opacity += 0.1;
+        this.style.backgroundColor = `rgba(0, 0, 0, ${opacity})`;
+      }
+    } else {
+        this.style.backgroundColor = `rgba(0, 0, 0, 0.1)`;
+    }
+  } else {
+    if (this.style.backgroundColor.match(/rgba/)) {
+      return;
+    } else {
+      let r = Math.floor(255*Math.random());
+      let g = Math.floor(255*Math.random());
+      let b = Math.floor(255*Math.random());
+      let a = Math.random();
+      this.style.backgroundColor = `rgba(${r}, ${g}, ${b}, ${a})`;
+    }
+  }
+}
+
 function resetBoard() {
   arrayDiv.forEach(div => div.removeAttribute('style'));
 }
 
-// Resize Board
 function changeSize() {
+  sizeNumber = prompt('Choose the number of rows/columns of the board', 'Max. allowed: 60');
   container.textContent = '';
   arrayDiv = [];
-  sizeNumber = prompt('Choose the number of rows/columns of the board', 'Max. allowed: 60');
   if (sizeNumber > 60) {
     sizeNumber = 60;
   }
